8bdeeb031621671b8546a163dec5e4f78e0c071f
mori-kaito <mkaito4210@gmail.com>
2023-11-14 18:47:48 +0900

    さわり

--- kuchikomi.py
+++ kuchikomi.py
@@ -1,14 +1,47 @@
 from bigchaindb_driver import BigchainDB
+from pymongo import MongoClient
 from bigchaindb_driver.crypto import generate_keypair
+from datetime import datetime
 
 conn = BigchainDB('http://localhost:9984/')
 
-nameSpace = 'rbac-bdb-kuchikomi'
-async def createApp():
-    admin = generate_keypair()
+class kuchikomi(object):
 
-    adminGroupAsset = conn.transactions.prepare(
-        operation='CREATE',
-        signers=admin.public_key,
-    )
+    def __init__(self):
+         self.clint = MongoClient()
+         self.db = self.clint['kuchikomi']
+
+    def add_one(self, title, text):
+        id = self.db.kuchikomi.count_documents()+1
+        """データ挿入"""
+        post = {
+            'id': id,
+            'title': title,
+            'content': text,
+            'created_at': datetime.now(),
+            'delete': 0
+        }
+        return self.db.kuchikomi.insert_one(post)
+    
+    def get_one(self, id):
+        return self.db.kuchikomi.find(filter = {'id': id})
+    
+    def update(self, id, text):
+        rest = self.db.kuchikomi.update_one({"id": id}, {"$set": {"content": text, 'created_at': datetime.now()}})
+        return rest
+    
+    def delete(self, id):
+        """マッチした最初のデータを削除"""
+        rest = self.db.kuchikomi.update_one({"title": id}, {"$set": {"delete": 1}})
+        return rest
+
+def main():
+    title = input("タイトル：")
+    text = input("口コミの内容：")
+    obj = kuchikomi()
+    rest = obj.add_one(title, text)
+    rest = obj.get_one(1)
+    print(rest)
+
+# kaito = generate_keypair()
 
---
+++ mongo.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+class TestMongo(object):
+
+    def __init__(self):
+         self.clint = MongoClient()
+         self.db = self.clint['test']
+
+    def add_one(self):
+        """データ挿入"""
+        post = {
+            'title': 'ハリネズミ',
+            'content': 'ハリネズミ可愛い~',
+            'created_at': datetime.now()
+        }
+        return self.db.test.insert_one(post)
+    
+    def get_one(self):
+        return self.db.test.find_one()
+
+def main():
+    obj = TestMongo()
+    rest = obj.get_one()
+    print(rest)
+
+if __name__ == '__main__':
+    main()
